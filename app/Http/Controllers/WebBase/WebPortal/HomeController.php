b772be9ff050a5598fe355763bdddb5db1776f8c
frandika06 <frandika.septa@gmail.com>
2023-07-27 04:30:35 +0700

    Update Portal->Beranda 270723 04.30

--- app/Http/Controllers/WebBase/WebPortal/HomeController.php
+++ app/Http/Controllers/WebBase/WebPortal/HomeController.php
@@ -11,6 +11,15 @@ class HomeController extends Controller
     {
         $response = Http::get('https://api.slingacademy.com/v1/sample-data/blog-posts?limit=3');
         $posts = $response->object();
-        return view('pages.portal.home.index', compact('posts'));
+        // IMG GALLERI 1
+        $offset = rand(0, 100);
+        $endpoint1 = "https://api.slingacademy.com/v1/sample-data/photos?offset=$offset&limit=5";
+        $response1 = Http::get($endpoint1);
+        $images1 = $response1->object();
+        $images1 = $images1->photos;
+        return view('pages.portal.home.index', compact(
+            'posts',
+            'images1'
+        ));
     }
 }
--- resources/views/pages/portal/home/index.blade.php
+++ resources/views/pages/portal/home/index.blade.php
@@ -14,7 +14,7 @@
             data-dynamic-height="['700px','700px','700px','550px','500px']" style="height: 700px;">
             <div class="owl-stage-outer">
                 <div class="owl-stage">
-                    <!-- Carousel Slide 1 -->
+                    {{-- Carousel Slide 1 --}}
                     <div class="owl-item position-relative" style="background-image: url({{ asset('assets-portal/dist/img/parallax/parallax-04.png') }}); background-size: cover; background-position: center;">
                         <div class="container position-relative z-index-1 h-100">
                             <div class="row justify-content-center align-items-center h-100">
@@ -28,48 +28,20 @@
                             </div>
                         </div>
                     </div>
-                    <!-- Carousel Slide 1 -->
-                    <div class="owl-item position-relative" style="background-image: url({{ asset('assets-portal/dist/img/parallax/parallax-04.png') }}); background-size: cover; background-position: center;">
-                        <div class="container position-relative z-index-1 h-100">
-                            <div class="row justify-content-center align-items-center h-100">
-                                <div class="col-lg-7">
-                                    <div class="d-flex flex-column align-items-center">
-                                        <h2 class="text-color-dark font-weight-extra-bold text-10 text-md-12-13 line-height-1 text-center mb-2 appear-animation" data-appear-animation="blurIn" data-appear-animation-delay="500" data-plugin-options="{'minWindowWidth': 0}">SELAMAT DATANG
-                                        </h2>
-                                        <p class="text-4-5 text-color-dark font-weight-light text-center mb-4" data-plugin-animated-letters data-plugin-options="{'startDelay': 1000, 'minWindowWidth': 0, 'animationSpeed': 30}">Di Website Pelayanan Tera / Tera Ulang dan Pengujian BDKT</p>
+                    @foreach ($images1 as $item)
+                        <div class="owl-item position-relative" style="background-image: url({{ $item->url }}); background-size: cover; background-position: center;">
+                            <div class="container position-relative z-index-1 h-100">
+                                <div class="row justify-content-center align-items-center h-100">
+                                    <div class="col-lg-7">
+                                        <div class="d-flex flex-column align-items-center">
+                                            <h2 class="text-color-white font-weight-extra-bold text-10 text-md-12-13 line-height-1 text-center mb-2 appear-animation" data-appear-animation="blurIn" data-appear-animation-delay="500" data-plugin-options="{'minWindowWidth': 0}">{{ $item->title }}</h2>
+                                            <p class="text-4-5 text-color-white font-weight-light text-center mb-4" data-plugin-animated-letters data-plugin-options="{'startDelay': 1000, 'minWindowWidth': 0, 'animationSpeed': 30}">{{ $item->description }}</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <!-- Carousel Slide 1 -->
-                    <div class="owl-item position-relative" style="background-image: url({{ asset('assets-portal/dist/img/parallax/parallax-04.png') }}); background-size: cover; background-position: center;">
-                        <div class="container position-relative z-index-1 h-100">
-                            <div class="row justify-content-center align-items-center h-100">
-                                <div class="col-lg-7">
-                                    <div class="d-flex flex-column align-items-center">
-                                        <h2 class="text-color-dark font-weight-extra-bold text-10 text-md-12-13 line-height-1 text-center mb-2 appear-animation" data-appear-animation="blurIn" data-appear-animation-delay="500" data-plugin-options="{'minWindowWidth': 0}">SELAMAT DATANG
-                                        </h2>
-                                        <p class="text-4-5 text-color-dark font-weight-light text-center mb-4" data-plugin-animated-letters data-plugin-options="{'startDelay': 1000, 'minWindowWidth': 0, 'animationSpeed': 30}">Di Website Pelayanan Tera / Tera Ulang dan Pengujian BDKT</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Carousel Slide 1 -->
-                    <div class="owl-item position-relative" style="background-image: url({{ asset('assets-portal/dist/img/parallax/parallax-04.png') }}); background-size: cover; background-position: center;">
-                        <div class="container position-relative z-index-1 h-100">
-                            <div class="row justify-content-center align-items-center h-100">
-                                <div class="col-lg-7">
-                                    <div class="d-flex flex-column align-items-center">
-                                        <h2 class="text-color-dark font-weight-extra-bold text-10 text-md-12-13 line-height-1 text-center mb-2 appear-animation" data-appear-animation="blurIn" data-appear-animation-delay="500" data-plugin-options="{'minWindowWidth': 0}">SELAMAT DATANG
-                                        </h2>
-                                        <p class="text-4-5 text-color-dark font-weight-light text-center mb-4" data-plugin-animated-letters data-plugin-options="{'startDelay': 1000, 'minWindowWidth': 0, 'animationSpeed': 30}">Di Website Pelayanan Tera / Tera Ulang dan Pengujian BDKT</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    @endforeach
                 </div>
             </div>
             <div class="owl-nav">
